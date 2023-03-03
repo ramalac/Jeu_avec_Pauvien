7af0020d310ff2282b4b39bd7947ed4bfc7d8b6e
Pauline Hospital <Pauline.HOSPITAL2@etu.uca.fr>
2023-03-04 00:53:28 +0100

    Gestion collision herbe et fil

---
+++ Assets/Script/Etendoir.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Etendoir : MonoBehaviour
+{
+    public GameObject HerbeSeche;
+
+    public float count_down_time = 10.0f; //1 minute
+    private bool count_down;
+    private float initial_value = 0;
+    private GameObject herbe;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        count_down = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (count_down)
+        {
+            if (count_down_time > 0)
+            {
+                count_down_time -= Time.deltaTime;
+            }
+            else
+            {
+                Vector3 myVector3 = herbe.transform.position;
+                Destroy(herbe);
+
+                GameObject herbeSechee = Instantiate(HerbeSeche, myVector3, Quaternion.identity);
+            }
+        }
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        //Check for a match with the specified name on any GameObject that collides with your GameObject
+        if (other.gameObject.name == "Herbe")
+        {
+            Debug.Log("Collision !");
+            count_down = true;
+            initial_value = count_down_time;
+            herbe = other.gameObject;
+        }
+    }
+}
