54f21a312baeb4d99b5e9254489b4f2830b70018
Pauline Hospital <Pauline.HOSPITAL2@etu.uca.fr>
2023-03-04 09:36:40 +0100

    Ajout des commentaires

--- Assets/Script/Etendoir.cs
+++ Assets/Script/Etendoir.cs
@@ -4,50 +4,69 @@ using UnityEngine;
 
 public class Etendoir : MonoBehaviour
 {
-    public GameObject HerbeSeche;
+    public GameObject herbeSeche;
 
     public float count_down_time = 10.0f; //1 minute
     private bool count_down;
-    private float initial_value = 0;
     private GameObject herbe;
 
     // Start is called before the first frame update
     void Start()
     {
         count_down = false;
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+        //If the count down has started
         if (count_down)
         {
+            //If the count down is still turning 
             if (count_down_time > 0)
             {
+                //We subtract time
                 count_down_time -= Time.deltaTime;
             }
+            //If the count down is finished
             else if(herbe != null)
             {
+                //We get the current position of the grass
                 Vector3 myVector3 = herbe.transform.position;
+                //We destroy the grass
                 Destroy(herbe);
                 herbe = null;
 
-                GameObject herbeSechee = Instantiate(HerbeSeche, myVector3, Quaternion.identity);
+                //We instantiate a new dried grass
+                GameObject herbeSechee = Instantiate(herbeSeche, myVector3, Quaternion.identity);
             }
         }
         
     }
+
+    //incoming collision management
     private void OnTriggerEnter(Collider other)
     {
         //Check for a match with the specified name on any GameObject that collides with your GameObject
         if (other.gameObject.name == "Herbe")
         {
-            Debug.Log("Collision !");
+            //Start a count down
             count_down = true;
-            initial_value = count_down_time;
+
+            //Stock the grass object and set it to kinematic
             herbe = other.gameObject;
             herbe.GetComponent<Rigidbody>().isKinematic = true;
+        }
+    }
+
+    //outcoming collision management
+    private void OnTriggerExit(Collider other)
+    {
+        //Check for a match with the specified name on any GameObject that collides with your GameObject
+        if (other.gameObject.name == "Herbe")
+        {
+            //If the grass is no longer on the rack the count_down restart
+            count_down_time = 10.0f;
         }
     }
 }
