2c8da48a345b3823ee0eacbc73508fa817593a8b
Flavien63 <flavien.dermigny@gmail.com>
2023-03-04 18:54:11 +0100

    Mise à jour, fonctionnalité de l'arc et des plaches

---
+++ Assets/Script/Bucheron.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Bucheron : MonoBehaviour
+{
+    public GameObject souche;
+    public GameObject planche;
+    private int nbCout;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nbCout = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Wood"))
+        {
+            nbCout++;
+        }
+        if (nbCout == 3)
+        {
+            nbCout = 0;
+            Vector3 vector3 = collision.gameObject.transform.position;
+            Destroy(collision.gameObject);
+            GameObject hs = Instantiate(souche, this.transform.position, Quaternion.identity);
+            GameObject hs1 = Instantiate(planche, vector3, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Script/tableCraft.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class tableCraft : MonoBehaviour
+{
+    private bool yarnOnTable;
+    private bool woodenBoardOnTable;
+    private GameObject yarn;
+    private GameObject wood;
+    public GameObject arrow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        yarnOnTable = false;
+        woodenBoardOnTable = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (yarnOnTable && woodenBoardOnTable)
+        {
+            Vector3 vector3 = wood.transform.position;
+            //We destroy the yarn and wood
+            Destroy(yarn);
+            Destroy(wood);
+            yarn = null;
+            wood = null;
+            yarnOnTable = false;
+            woodenBoardOnTable = false;
+
+            //We instantiate a new arrow
+            GameObject hs = Instantiate(arrow, vector3, Quaternion.identity);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("collision");
+        if (collision.gameObject.tag != null)
+            Debug.Log(collision.gameObject.tag);
+
+        //Check if this is a wooden board
+        if (collision.gameObject.CompareTag("WoodenBoard"))
+        {
+            //Check if is the top of the table
+            if (collision.transform.position.y > transform.position.y)
+            {
+                woodenBoardOnTable = true;
+                wood = collision.gameObject;
+            }
+        }
+
+        //Check if this is a yarn 
+        if (collision.gameObject.CompareTag("Yarn"))
+        {
+            //Check if is the top of the table
+            if (collision.transform.position.y > transform.position.y)
+            {
+                yarnOnTable = true;
+                yarn = collision.gameObject;
+            }
+        }
+    }
+}
