c600a89c1f7bd29e99c5861bc90f2fbddbc94952
Pauline Hospital <Pauline.HOSPITAL2@etu.uca.fr>
2023-03-05 09:52:34 +0100

    Recettes

---
+++ Assets/Scripts/startRules.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startRules : MonoBehaviour
+{
+    private GameObject reglesPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //reglesPanel = transform.Find("ReglesPanel").gameObject;
+        //reglesPanel.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (Input.GetButtonDown("A"))
+        {
+            reglesPanel.SetActive(false);
+        }*/
+    }
+}
